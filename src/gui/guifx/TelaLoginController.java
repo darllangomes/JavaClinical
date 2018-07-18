d0ed482c2d9004adede1e635e0813a755b87adbd
Danilo Araújo <daniloaraujo1990@hotmail.com>
2018-07-18 17:38:06 -0300

    Ajustes de Projeto

--- src/gui/guifx/TelaLoginController.java
+++ src/gui/guifx/TelaLoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication1;
+package gui.javafx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/gui/guifx/src/teladoinicial/TelaDoInicial.java
+++ src/gui/guifx/src/teladoinicial/TelaDoInicial.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package teladoinicial;
+package gui.guifx.src.teladoinicial;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
